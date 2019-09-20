c317c6ba95664216d7b1227aa2ff22fed1d84b77
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-20 02:31:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
+function increaseRankBy(n) {
+    const lis = document.querySelectorAll("ul.ranked-list li");
+    for (let i = 0; i < lis.length; i++) {
+        lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+    }
+}
+
 function deepestChild() {
     const divs = document.querySelectorAll("#grand-node div");
     return divs[divs.length - 1]
 }
-function increaseRankBy(n)
